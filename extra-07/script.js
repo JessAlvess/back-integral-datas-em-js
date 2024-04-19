59c654bcce356476fd572cc6b57192899de30ba3
Jessé Alves <jessealvespr@outlook.com>
2024-04-18 21:17:25 -0300

    feat: extra7 finalizado

--- extra-07/script.js
+++ extra-07/script.js
@@ -1,14 +1,14 @@
-import { addHours } from "date-fns";
+import { addDays } from "date-fns";
 
 function promocao(dateInicio, dateSolicitacao) {
   const dataInicio = new Date(dateInicio);
-  const dataFinal = addHours(dataInicio, 24);
+  const dataFinal = addDays(dataInicio, 30);
   const dataSolicitacao = new Date(dateSolicitacao);
 
   return dataSolicitacao.getTime() >= dataInicio.getTime() &&
-    dataSolicitacao.getTime() <= dataFinal.getTime()
+    dataSolicitacao.getTime() < dataFinal.getTime()
     ? console.log(true)
     : console.log(false);
 }
 
-promocao(new Date(2024, 3, 18, 15), new Date(2024, 3, 19, 14, 1));
+promocao(new Date(2024, 3, 1), new Date(2024, 3, 30));
